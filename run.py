e1a65699edae35305990b3c65545c50a29ae04a8
ilyushin <john.ilyushin@gmail.com>
2016-03-16 19:17:41 +0300

    Added printing result to csv.

--- run.py
+++ run.py
@@ -1,8 +1,12 @@
 #!/usr/bin/python2.7
 
-import os, sys
+import os, sys, subprocess, time, csv
 
+# set path to a main directory
 main_dir = os.path.dirname(__file__)
+
+# set path to the closure
+path_closure = os.path.join(main_dir, 'compiler.jar')
 
 # set path to tests
 path_tests = os.path.join(main_dir, 'tests')
@@ -21,15 +25,51 @@ def get_size(path_to_file):
 
 class TestFile:
     def __init__(self, path_to_file):
+        self.file_name = os.path.splitext(os.path.basename(path_to_file))[0]
         self.path_to_file = path_to_file
-        self.size_before = get_size(path_to_file)
-        self.size_after = 0
+        self.perc_dec_size = 0  # calculation of percentage decrease size
+        self.perc_dec_time = 0  # calculation of percentage decrease execution time
 
 
 tests = []
 for fn in os.listdir(path_tests):
-    if fn.lower().endswith('.js'):
+    if fn.lower().find('opt') == -1 and fn.lower().endswith('.js'):
         tests.append(TestFile(path_tests + '/' + fn))
 
+for test_file in tests:
 
-print tests
+    size_before = get_size(test_file.path_to_file)
+    opt_file = os.path.join(path_tests, test_file.file_name + '_opt.js')
+    cmd_closure = 'java -jar ' + path_closure + ' --compilation_level ADVANCED_OPTIMIZATIONS' + ' --js ' + test_file.path_to_file + ' --js_output_file ' + opt_file
+    r1 = subprocess.call(cmd_closure, shell=True)
+    size_after = get_size(opt_file)
+
+    if size_before > size_after:
+        test_file.perc_dec_size = 100 - (size_after * 100 / size_before)
+
+    # execute an unoptimized test
+    start = time.time()
+    cmd_node = 'node ' + test_file.path_to_file
+    end = time.time()
+    time1 = end - start
+
+    # execute an optimized test
+    start = time.time()
+    cmd_node = 'node ' + opt_file
+    end = time.time()
+    time2 = end - start
+
+    if time2 < time1:
+        test_file.perc_dec_time = 100 - (time2 * 100 / time1)
+
+csvfile = open(path_results + '/result.csv', 'w+')
+csv_writer = csv.writer(csvfile, delimiter=' ', quoting=csv.QUOTE_NONE, escapechar=' ', quotechar='')
+csv_writer.writerow(['test', ';dec size(%)', ';dec time(%)'])
+for test_file in tests:
+    newStr = []
+    newStr.append(test_file.file_name)
+    newStr.append(';' + str(test_file.perc_dec_size))
+    newStr.append(';' + str(test_file.perc_dec_time))
+    csv_writer.writerow(newStr)
+csvfile.close()
+print "Finish"
