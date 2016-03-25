8f413cd7c3f49deb447d57c4598d7d7778acc6bd
ilyushin <john.ilyushin@gmail.com>
2016-03-25 10:47:46 +0300

    Added calculation of negative numbers.

--- run.py
+++ run.py
@@ -29,6 +29,10 @@ class TestFile:
         self.path_to_file = path_to_file
         self.perc_dec_size = 0  # calculation of percentage decrease size
         self.perc_dec_time = 0  # calculation of percentage decrease execution time
+        self.size_before = 0
+        self.size_after = 0
+        self.exec_time_before = 0
+        self.exec_time_after = 0
 
 
 tests = []
@@ -41,44 +45,50 @@ for fn in os.listdir(path_tests):
 
 for test_file in tests:
 
-    size_before = get_size(test_file.path_to_file)
+    test_file.size_before = get_size(test_file.path_to_file)
     opt_file = os.path.join(path_tests, test_file.file_name + '_opt.js')
     cmd_closure = 'java -jar ' + path_closure + ' --compilation_level ADVANCED_OPTIMIZATIONS' + ' --js ' + test_file.path_to_file + ' --js_output_file ' + opt_file
     r1 = subprocess.call(cmd_closure, shell=True)
-    size_after = get_size(opt_file)
+    test_file.size_after = get_size(opt_file)
 
-    if size_before > size_after:
-        test_file.perc_dec_size = 100 - (size_after * 100 / size_before)
+    if test_file.size_before > test_file.size_after:
+        test_file.perc_dec_size = test_file.size_after * 100 / test_file.size_before
 
     # execute an unoptimized test
     start = time.time()
     cmd_node = 'node ' + test_file.path_to_file
     end = time.time()
-    time1 = end - start
+    test_file.exec_time_before = end - start
 
     # execute an optimized test
     start = time.time()
     cmd_node = 'node ' + opt_file
     end = time.time()
-    time2 = end - start
+    test_file.exec_time_after = end - start
 
-    if time2 < time1:
-        test_file.perc_dec_time = 100 - (time2 * 100 / time1)
+    if test_file.exec_time_after < test_file.exec_time_before:
+        test_file.perc_dec_time = test_file.exec_time_after * 100 / test_file.exec_time_before
+    else:
+        test_file.perc_dec_time = -(test_file.exec_time_before * 100 / test_file.exec_time_after)
 
 csvfile = open(path_results + '/result.csv', 'w+')
 csv_writer = csv.writer(csvfile, delimiter=' ', quoting=csv.QUOTE_NONE, escapechar=' ', quotechar='')
-csv_writer.writerow(['test', ';dec size(%)', ';dec time(%)'])
+csv_writer.writerow(['test', ';dec size(%)', ';dec time(%)', ';size before', ';size after', ';exec time before', ';exec time after'])
 all_time, all_size, amount = 0, 0, 0
 
 for test_file in tests:
     newStr = []
-    newStr.append(test_file.file_name)
+    newStr.append(test_file.file_name+".js")
     newStr.append(';' + str(test_file.perc_dec_size))
-    newStr.append(';' + str(test_file.perc_dec_time))
+    newStr.append(';' + str(round(test_file.perc_dec_time,0))[:-2])
+    newStr.append(';' + str(test_file.size_before))
+    newStr.append(';' + str(test_file.size_after))
+    newStr.append(';' + str(test_file.exec_time_before))
+    newStr.append(';' + str(test_file.exec_time_after))
     csv_writer.writerow(newStr)
     all_time += test_file.perc_dec_time
     all_size += test_file.perc_dec_size
     amount += 1
-csv_writer.writerow(['', ';'+str(all_size/amount), ';'+str(all_time/amount)])
+csv_writer.writerow(['', ';'+str(all_size/amount), ';'+str(round(all_time/amount))[:-2]])
 csvfile.close()
 print "Finish"
