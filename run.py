034f54bcf55eea1b8b569991468ae172c0f76119
Evgeniy Ilyushin <john.ilyushin@gmail.com>
2016-03-29 11:28:13 +0300

    Updated results and fixed bugs.

--- run.py
+++ run.py
@@ -1,6 +1,16 @@
 #!/usr/bin/python2.7
 
-import os, sys, subprocess, time, csv
+import os, sys, subprocess, time, csv, timeit
+
+#set path to JavaScript engine and name of a resulting file
+path_v8 = ""
+result_name = ""
+if len(sys.argv) > 1 and sys.argv[1] == "-n":
+    path_v8 = "node"
+    result_name = "/result_node.csv"
+else:
+    path_v8 = "/Users/Evgeniy/Development/git/v8/out/x64.release/shell"
+    result_name = "/result_v8.csv"
 
 # set path to a main directory
 main_dir = os.path.dirname(__file__)
@@ -17,6 +27,7 @@ if not os.path.exists(path_tests):
 path_results = os.path.join(main_dir, 'results')
 if not os.path.exists(path_tests):
     os.makedirs(path_results)
+
 
 
 def get_size(path_to_file):
@@ -45,9 +56,13 @@ for fn in os.listdir(path_tests):
 
 for test_file in tests:
 
+    print 'START************************************************************************'
+    print str(test_file.path_to_file)
+    print '************************************************************************'
+
     test_file.size_before = get_size(test_file.path_to_file)
     opt_file = os.path.join(path_tests, test_file.file_name + '_opt.js')
-    cmd_closure = 'java -jar ' + path_closure + ' --compilation_level ADVANCED_OPTIMIZATIONS' + ' --js ' + test_file.path_to_file + ' --js_output_file ' + opt_file
+    cmd_closure = 'java -jar ' + path_closure + ' --compilation_level ADVANCED_OPTIMIZATIONS --summary_detail_level 0' + ' --js ' + test_file.path_to_file + ' --js_output_file ' + opt_file
     r1 = subprocess.call(cmd_closure, shell=True)
     test_file.size_after = get_size(opt_file)
 
@@ -55,23 +70,31 @@ for test_file in tests:
         test_file.perc_dec_size = test_file.size_after * 100 / test_file.size_before
 
     # execute an unoptimized test
-    start = time.time()
-    cmd_node = 'node ' + test_file.path_to_file
+    start = time.time()    
+    cmd_v8 = path_v8+' '+ test_file.path_to_file
+    subprocess.call(cmd_v8, shell=True, stderr=subprocess.STDOUT)
     end = time.time()
-    test_file.exec_time_before = end - start
+    
+    test_file.exec_time_before = (end - start)
 
     # execute an optimized test
     start = time.time()
-    cmd_node = 'node ' + opt_file
+    cmd_v8 = path_v8+' '+ opt_file
+    subprocess.call(cmd_v8, shell=True, stderr=subprocess.STDOUT)
     end = time.time()
-    test_file.exec_time_after = end - start
+    
+    test_file.exec_time_after = (end - start)
 
     if test_file.exec_time_after < test_file.exec_time_before:
         test_file.perc_dec_time = test_file.exec_time_after * 100 / test_file.exec_time_before
     else:
         test_file.perc_dec_time = -(test_file.exec_time_before * 100 / test_file.exec_time_after)
+        
+    print 'FINISH  **********************************************************************'
+    print test_file.path_to_file
+    print '_______________________________________________________________________________'
 
-csvfile = open(path_results + '/result.csv', 'w+')
+csvfile = open(path_results + result_name, 'w+')
 csv_writer = csv.writer(csvfile, delimiter=' ', quoting=csv.QUOTE_NONE, escapechar=' ', quotechar='')
 csv_writer.writerow(['test', ';dec size(%)', ';dec time(%)', ';size before', ';size after', ';exec time before', ';exec time after'])
 all_time, all_size, amount = 0, 0, 0
